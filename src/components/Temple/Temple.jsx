c2c8003ceffaf08ebde2cdd545e03d9723a65ed5
Lakshya <iamlakshya18@gmail.com>
2023-05-17 19:33:19 +0530

    fam

--- src/components/Temple/Temple.jsx
+++ src/components/Temple/Temple.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import back from "./Templeback.jpg"
 import TempleCard from './TempleCard'
-import {Link } from 'react-router-dom'
+// import {Link } from 'react-router-dom'
 function Temples() {
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
 
